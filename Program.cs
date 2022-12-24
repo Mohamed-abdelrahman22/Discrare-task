18d4cc0d2debc7422d9350e39a66ba1abfbd31a7
Mohamed-abdelrahman22 <121330401+Mohamed-abdelrahman22@users.noreply.github.com>
2022-12-24 07:53:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ThePerfectNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The First Number : ");
+            int first = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter The Second Number : ");
+            int second = int.Parse(Console.ReadLine());
+            Console.WriteLine("The Perfect Numbers within the given range : ");
+            for (int y = first; y <= second; y++)
+            {
+             int x = 1;
+             int z = 0;
+             while (x < y)
+             {
+              if (y % x == 0)
+              z = z + x;
+              x++;
+             }
+              if (z == y)
+              Console.WriteLine(" {0} ", y);
+            }
+
+        }
+    }
+}
